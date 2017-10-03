c637398804e37a8627cefbb2409deac6af8faf2a
Jordan Gardner <jordanthomasg@gmail.com>
2017-10-02 21:34:30 -0600

    Add voting view
    Add the ability for users to see presidential candidiates and then
    place a vote.

--- election/urls.py
+++ election/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+import vote.views
 
 urlpatterns = [
+    url(r'^$', vote.views.Vote.as_view()),
     url(r'^admin/', admin.site.urls),
 ]
--- vote/views.py
+++ vote/views.py
@@ -1,3 +1,41 @@
+from django import forms
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.views import View
+from vote import models
 
-# Create your views here.
+
+class Vote(View):
+    '''Vote view.'''
+
+    def get(self, request):
+        '''Show the voting page.'''
+        candidates = models.Candidate.objects.all()
+        user_vote = request.session.get('vote', None)
+
+        return render(request, 'vote.html', {
+            'candidates': candidates,
+            'vote': user_vote,
+            })
+
+    def post(self, request):
+        '''Get the user's vote.'''
+        ballot = Ballot(request.POST)
+        if ballot.is_valid():
+            ballot.candidate.votes += 1
+            ballot.candidate.save()
+            request.session['vote'] = ballot.candidate.id
+        return HttpResponseRedirect('/')
+
+
+class Ballot(forms.Form):
+    '''Voter ballot.'''
+    candidate_id = forms.IntegerField(min_value=0)
+
+    def clean(self):
+        if len(self.errors) == 0:
+            try:
+                self.candidate = models.Candidate.objects.get(id=self.cleaned_data.get('candidate_id'))
+            except models.Candidate.DoesNotExist:
+                raise forms.ValidationError('Invalid candidate id.')
+        return self.cleaned_data
